e015f244917ccbf73fe05482514c67e0c3c62d4a
hanyue <47289292+hanyue1014@users.noreply.github.com>
2022-09-16 17:07:16 +0800

    Update styles and logic
    1. Update user action menu styles
    2. Update how the user action menu should behave

--- js/common.js
+++ js/common.js
@@ -1,55 +1,11 @@
-// This file contains functionality that will share across html files
-
-let header = document.querySelector('header')
-// to determine whether to add and remove the collapsed class or not
-let headerIsFloating = false
-function collapseHeader() {
-  console.log(headerIsFloating)
-  if (headerIsFloating) {
-    header.classList.add('collapsed')
-  }
-}
-
-function expandHeader() {
-  if (headerIsFloating) {
-    header.classList.remove('collapsed')
-  }
-}
-
-/*
-Apply the "shrink and float header bar"
-- wanted to use intersection observer for optimised performance 
-  but that would add a little complexity 
-  and not all browsers support the intersection observer (some outdated didnt support)
-- make the window listen for onscroll function
-- check the scroll on body and the documentElement AKA html itself
-- if the scrollTop is big, apply the class designed for the scrolled header
-*/
-// TODO: Create a button with id goToTop, give it the ability to scroll to the top when clicked
-window.onscroll = () => {
-  if (document.body.scrollTop >= 40 || document.documentElement.scrollTop >= 40) {
-    headerIsFloating = true
-    header.classList.add('collapsed', 'floating')
-    // document.getElementById('goToTop').style.display = 'block'
-  } else {
-    // if the scroll is not that big then the class shall be removed
-    headerIsFloating = false
-    header.classList.remove('collapsed', 'floating')
-    // document.getElementById('goToTop').style.display = 'none'
-  }
-}
-// for some reason focus and focusout doesn't seem to be working, will just leave it here
-addMultipleEventListeners(header, collapseHeader, "mouseleave", "focusout")
-addMultipleEventListeners(header, expandHeader, "mouseenter", "focus")
-
-// log in stuffs
 // if user is logged in, this will be a string with smtg inside, if not it will be null
 let isLoggedIn = sessionStorage.getItem('isLoggedIn')
 
 const userMenuBtn = document.querySelector('#user-menu')
 let isUserMenuOpen = false;
 let openedMenuId = '';
-userMenuBtn.addEventListener("click", () => {
+
+function toggleMenuActionShow() {
   const userActionMenus = document.querySelectorAll('.user-menu-actions')
   userActionMenus.forEach(userActMenu => {
     // check if the menu is already opened, if it is, close it
@@ -78,20 +34,66 @@ userMenuBtn.addEventListener("click", () => {
       }
     }
   })
-})
+}
+
+userMenuBtn.addEventListener("click", toggleMenuActionShow)
 
 // if click outside the userMenuBtn, check if the menu is open and if is open, close it
 document.addEventListener('click', event => {
-  const isClickInside = userMenuBtn.contains(event.target)
+  const isClickInsideUserMenu = userMenuBtn.contains(event.target)
 
-  if (!isClickInside) {
-    // select the opened element
-    const openedMenu = document.getElementById(openedMenuId)
-    if (isUserMenuOpen) {
-      openedMenu.classList.remove('show-user-menu-actions')
-      openedMenu.style.display = 'none'
-      isUserMenuOpen = false
-      openedMenuId = ''
-    }
+  if (!isClickInsideUserMenu) {
+    toggleMenuActionShow()
   }
 })
+
+/*
+Apply the "shrink and float header bar"
+- wanted to use intersection observer for optimised performance 
+  but that would add a little complexity 
+  and not all browsers support the intersection observer (some outdated didnt support)
+- make the window listen for onscroll function
+- check the scroll on body and the documentElement AKA html itself
+- if the scrollTop is big, apply the class designed for the scrolled header
+*/
+// TODO: Create a button with id goToTop, give it the ability to scroll to the top when clicked
+window.onscroll = () => {
+  if (document.body.scrollTop >= 40 || document.documentElement.scrollTop >= 40) {
+    headerIsFloating = true
+    header.classList.add('collapsed', 'floating')
+    // document.getElementById('goToTop').style.display = 'block'
+  } else {
+    // if the scroll is not that big then the class shall be removed
+    headerIsFloating = false
+    header.classList.remove('collapsed', 'floating')
+    // document.getElementById('goToTop').style.display = 'none'
+  }
+  // unshow the user menu action on scroll also
+  if (isUserMenuOpen) {
+    toggleMenuActionShow()
+  }
+}
+
+let header = document.querySelector('header')
+// to determine whether to add and remove the collapsed class or not
+let headerIsFloating = false
+function collapseHeader() {
+  console.log(headerIsFloating)
+  if (headerIsFloating) {
+    header.classList.add('collapsed')
+    // unshow the user menu action when the header collapse also
+    if (isUserMenuOpen) {
+      toggleMenuActionShow()
+    }
+  }
+}
+
+function expandHeader() {
+  if (headerIsFloating) {
+    header.classList.remove('collapsed')
+  }
+}
+
+// for some reason focus and focusout doesn't seem to be working, will just leave it here
+addMultipleEventListeners(header, collapseHeader, "mouseleave", "focusout")
+addMultipleEventListeners(header, expandHeader, "mouseenter", "focus")

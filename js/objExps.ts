3c464d190ddabf0ae5a65acf0f162dc27095dc31
hanyue1014 <wongxuanbin@gmail.com>
2022-09-20 11:42:36 +0800

    Prototype the contents to be shown on MyAccount

--- js/objExps.ts
+++ js/objExps.ts
@@ -1,3 +1,5 @@
+// OMALORD THE TYPES ARE MESSY AF, but will make do since we cant have a real backend
+
 // an example of how the fake backend appears, will be in JSON when retrieved from localStorage and sessionStorage
 // will use ts to easier describe the types
 
@@ -12,3 +14,25 @@ interface User {
 
 // can retrieve the list of tour guides from localStorage, it will be a string array of their usernames
 type TourGuides = string[];
+
+// for premade tours
+// link is where the premade tour leads to in our page
+// name is the title of that premade tour
+interface PremadeTours {
+  link: string;
+  name: string;
+}
+
+// for custom made tours applied by user
+interface CustomMadeTours {
+  // TODO: decide which fields should this have
+}
+
+// for the tours applied by all customers
+// status whether the tour applied is accepted or pending
+// guide is username of the guide who accepted the tour
+// customer is the customer who applied the tour
+interface AppliedTours {
+  premade: (PremadeTours & { status: 'ACCEPTED' | 'PENDING'; guide: string; customer: string; })[];
+  customMade: (CustomMadeTours & { status: 'ACCEPTED' | 'PENDING'; guide: string; customer: string; })[];
+}

63c5d6038cc587794f0e1edfa476b64bcbfe9032
hanyue1014 <wongxuanbin@gmail.com>
2022-09-25 12:50:44 +0800

    Prototype resize

--- js/tour1Details.js
+++ js/tour1Details.js
@@ -1,3 +1,4 @@
 // this file exists solely to bind the image map to the resizable map
 const tour1ImgMap = document.getElementById('tour-1-img-map')
-new ResizableMap(tour1ImgMap, 1920, 1080)
+const tour1Img = document.getElementById('tour-1-img')
+new ResizableMap(tour1ImgMap, tour1Img, 1920, 1080)
--- js/tourDetails.js
+++ js/tourDetails.js
@@ -3,25 +3,36 @@ class ResizableMap {
   /**
    * Creates a resizable map that resizes the image map based on window size
    * @param {HTMLMapElement} map The map element which needs to be resized
+   * @param {HTMLImageElement} img The image that has been resized
    * @param {number} originalWidth Original width of the image bound to
    * @param {number} originalHeight Original height of the image bound to
    */
-  constructor(map, originalWidth, originalHeight) {
+  constructor(map, img, originalWidth, originalHeight) {
     console.log(this)
     // set this and trigger resize once (which will calculate and remap the coords
     this.previousWidth = originalWidth
     this.previousHeight = originalHeight
-  
+    this.map = map
+    this.img = img
+    
     this.resize = () => {
       console.log('resize triggered')
+      console.log(this)
       console.log('previous width:', this.previousWidth)
       console.log('previous height:', this.previousHeight)
-      console.log('map:', map)
+      console.log('map:', this.map)
+      console.log('img:', this.img)
+      // all the area tags of the map elements, converting it into array first
+      let areas = Array.from(this.map.areas)
+      areas.forEach(area => {
+        // TODO: handle resize logic here
+        console.log(area.coords)
+      })
     }
-  
+
     // trigger resize once to adjust the coords
     this.resize()
-  
+
     // trigger resize whenever window changes size
     window.addEventListener("resize", this.resize)
   }

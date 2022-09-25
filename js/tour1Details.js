e34c54686db2988350dfa7ca7129112bac5a2214
hanyue1014 <wongxuanbin@gmail.com>
2022-09-25 12:34:26 +0800

    Prototype responsive image map

---
+++ js/tour1Details.js
@@ -0,0 +1,3 @@
+// this file exists solely to bind the image map to the resizable map
+const tour1ImgMap = document.getElementById('tour-1-img-map')
+new ResizableMap(tour1ImgMap, 1920, 1080)
--- js/tourDetails.js
+++ js/tourDetails.js
@@ -1,1 +1,28 @@
 // functions for all those tour details
+class ResizableMap {
+  /**
+   * Creates a resizable map that resizes the image map based on window size
+   * @param {HTMLMapElement} map The map element which needs to be resized
+   * @param {number} originalWidth Original width of the image bound to
+   * @param {number} originalHeight Original height of the image bound to
+   */
+  constructor(map, originalWidth, originalHeight) {
+    console.log(this)
+    // set this and trigger resize once (which will calculate and remap the coords
+    this.previousWidth = originalWidth
+    this.previousHeight = originalHeight
+  
+    this.resize = () => {
+      console.log('resize triggered')
+      console.log('previous width:', this.previousWidth)
+      console.log('previous height:', this.previousHeight)
+      console.log('map:', map)
+    }
+  
+    // trigger resize once to adjust the coords
+    this.resize()
+  
+    // trigger resize whenever window changes size
+    window.addEventListener("resize", this.resize)
+  }
+}

e1074107168ad167c02769ca91b7fdc5d72d189d
AC3019 <55951264+AC3019@users.noreply.github.com>
2022-09-26 21:04:10 +0800

    create all files for tour3
    -add image into assets
    -html done
    -css havent start
    -js done

---
+++ js/tour3Details.js
@@ -0,0 +1,4 @@
+// this file exists solely to bind the image map to the resizable map
+const tour1ImgMap = document.getElementById('tour-3-img-map')
+const tour1Img = document.getElementById('tour-3-img')
+new ResizableMap(tour3ImgMap, tour3Img, 600, 450)

61615a75752fcf1d724fddedcda971787e8ab32a
hanyue1014 <wongxuanbin@gmail.com>
2022-10-02 18:01:41 +0800

    Add ratings selection to blogsform

---
+++ js/blogsForm.js
@@ -0,0 +1,39 @@
+const ratingsDiv = document.querySelector('.ratings-wrapper')
+
+// add selectRating function to all the children (the stars) of the rating div
+Array.from(ratingsDiv.children).forEach(el => el.addEventListener("click", selectRating))
+
+function selectRating(ev) {
+  console.log(ev.target.dataset.starIndex)
+  const selectedStar = ev.target.dataset.starIndex
+  // reset all the spans to empty star first
+  // get the current span's position 1 - 5, select all the spans before it and change it to filled star
+  Array.from(ratingsDiv.children).forEach(el => {
+    if (el.dataset.starIndex <= selectedStar)
+      el.innerHTML = '&#9733;'
+    else 
+      el.innerHTML = '&#9734;'
+  })
+  ratingsDiv.dataset.selected = selectedStar
+}
+
+function cancelinput() {
+  document.getElementById("myform").reset();
+}
+
+function myfunc(form, e) {
+  e.preventDefault();  // prevent redirect
+
+  if (!form.checkValidity()) {
+    form.reportValidity();
+  } else if (parseInt(ratingsDiv.dataset.selected) !== NaN && parseInt(ratingsDiv.dataset.selected) <= 0) {
+    // no stars selected
+    alert("Please select a star rating!");
+  } else {
+    var publish = confirm("Publish this blog?");
+    if (publish) {
+      alert("You have successfully published this blog");
+      goHome()
+    } else alert("You cancelled the operation");
+  }
+}
--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,7 @@
 /**
  * Adds multiple event listeners to an object
  * @param {HTMLElement | Window} el Element to apply the listener
- * @param {(el: HTMLElement, ev: Event) => void} fn function to run when listener triggered
+ * @param {(ev: Event) => void} fn function to run when listener triggered
  * @param  {...(WindowEventMap | HTMLElementEventMap)} listeners Listeners to be added
  */
 function addMultipleEventListeners(el, fn, ...listeners) {
